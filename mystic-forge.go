612d7dbbe8d9e9d20134cc8b5a29001fd7a3e7ee
Dylan Ashby <dylanashby@Dylans-MBP.lan>
2023-07-22 14:23:10 -0400

    first push

---
+++ mystic-forge.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+)
+
+type Equipment struct {
+	Level   int
+	HP      int
+	Attack  int
+	Defense int
+	// Add more stats as needed
+}
+
+type Monster struct {
+	HP      int
+	Attack  int
+	Defense int
+	// Add more stats as needed
+}
+
+type Helmet struct {
+	Equipment
+}
+
+type Pauldrons struct {
+	Equipment
+}
+
+type Gloves struct {
+	Equipment
+}
+
+type Chest struct {
+	Equipment
+}
+
+type Necklace struct {
+	Equipment
+}
+
+type LegArmour struct {
+	Equipment
+}
+
+type Ring struct {
+	Equipment
+}
+
+type Boots struct {
+	Equipment
+}
+
+func main() {
+	helmet := &Helmet{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      100,
+			Attack:  10,
+			Defense: 5,
+		},
+	}
+	pauldrons := &Pauldrons{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      50,
+			Attack:  5,
+			Defense: 3,
+		},
+	}
+	gloves := &Gloves{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      30,
+			Attack:  3,
+			Defense: 2,
+		},
+	}
+	chest := &Chest{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      200,
+			Attack:  15,
+			Defense: 8,
+		},
+	}
+	necklace := &Necklace{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      50,
+			Attack:  5,
+			Defense: 3,
+		},
+	}
+	legArmour := &LegArmour{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      100,
+			Attack:  10,
+			Defense: 5,
+		},
+	}
+	ring := &Ring{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      20,
+			Attack:  2,
+			Defense: 1,
+		},
+	}
+	boots := &Boots{
+		Equipment: Equipment{
+			Level:   1,
+			HP:      40,
+			Attack:  4,
+			Defense: 2,
+		},
+	}
+
+	// Initialize the game window
+	ebiten.SetWindowSize(800, 600)
+	ebiten.SetWindowTitle("Blacksmith Game")
+
+	// Load the background image
+	backgroundImg, _, _ := ebitenutil.NewImageFromFile("background.jpg", ebiten.FilterDefault)
+
+	// Implement game logic here
+	fmt.Println("Welcome to the blacksmith game!")
+
+	// Display the stats of different equipment types
+	fmt.Println("=== Equipment Stats ===")
+	fmt.Printf("Helmet - Level: %d, HP: %d, Attack: %d, Defense: %d\n", helmet.Level, helmet.HP, helmet.Attack, helmet.Defense)
+	fmt.Printf("Pauldrons - Level: %d, HP: %d, Attack: %d, Defense: %d\n", pauldrons.Level, pauldrons.HP, pauldrons.Attack, pauldrons.Defense)
+	fmt.Printf("Gloves - Level: %d, HP: %d, Attack: %d, Defense: %d\n", gloves.Level, gloves.HP, gloves.Attack, gloves.Defense)
+	fmt.Printf("Chest - Level: %d, HP: %d, Attack: %d, Defense: %d\n", chest.Level, chest.HP, chest.Attack, chest.Defense)
+	fmt.Printf("Necklace - Level: %d, HP: %d, Attack: %d, Defense: %d\n", necklace.Level, necklace.HP, necklace.Attack, necklace.Defense)
+	fmt.Printf("Leg Armour - Level: %d, HP: %d, Attack: %d, Defense: %d\n", legArmour.Level, legArmour.HP, legArmour.Attack, legArmour.Defense)
+	fmt.Printf("Ring - Level: %d, HP: %d, Attack: %d, Defense: %d\n", ring.Level, ring.HP, ring.Attack, ring.Defense)
+	fmt.Printf("Boots - Level: %d, HP: %d, Attack: %d, Defense: %d\n", boots.Level, boots.HP, boots.Attack, boots.Defense)
+
+	// Run the game loop
+	if err := ebiten.RunGame(&Game{Background: backgroundImg}); err != nil {
+		fmt.Println(err)
+	}
+}
+
+type Game struct {
+	Background *ebiten.Image
+}
+
+func (g *Game) Update() error {
+	// Game update logic here
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Draw the background image
+	screen.DrawImage(g.Background, nil)
+
+	// Additional drawing logic here
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	// Set the screen size
+	return 800, 600
+}
