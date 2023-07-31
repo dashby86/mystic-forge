f2da28cec5b9e45e163a7a36f358dbba63777856
Dylan Ashby <dylanashby@Dylans-MBP.lan>
2023-07-31 08:21:38 -0400

    schema wasnt comitted.

--- schema/starting-schema.sql
+++ schema/starting-schema.sql
@@ -33,6 +33,24 @@ CREATE TABLE IF NOT EXISTS `player_gear` (
     CONSTRAINT UC_player_slot UNIQUE (player_id, gear_slot_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `ore` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'pk',
+    `name` varchar(255) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `ore_inventory` (
+    `player_id` int(11) unsigned NOT NULL,
+    `quantity` int(11) unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY (`player_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `player_level` (
+    `player_id` int(11) unsigned NOT NULL,
+    `level` int(11) unsigned NOT NULL DEFAULT 1,
+    PRIMARY KEY (`player_id`)
+)
+
 INSERT IGNORE INTO `app`.`gear_slot` (`name`) VALUES ('Helmet');
 INSERT IGNORE INTO `app`.`gear_slot` (`name`) VALUES ('Pauldrons');
 INSERT IGNORE INTO `app`.`gear_slot` (`name`) VALUES ('Gloves');
@@ -47,3 +65,13 @@ INSERT IGNORE INTO `app`.`rarity` (`name`) VALUES ('Common');
 INSERT IGNORE INTO `app`.`rarity` (`name`) VALUES ('Uncommon');
 INSERT IGNORE INTO `app`.`rarity` (`name`) VALUES ('Rare');
 INSERT IGNORE INTO `app`.`rarity` (`name`) VALUES ('Epic');
+
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Copper');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Iron');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Nickle');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Tin');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Silver');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Gold');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Platinum');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Cobalt');
+INSERT IGNORE INTO `app`.`ore` (`name`) VALUES ('Mithril');
